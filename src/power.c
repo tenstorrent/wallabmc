6123e071bcf0731088b3d89345bc57feb33bf6b9
Michael Ellerman <mpe@tenstorrent.com>
2025-11-27 23:56:58 +1100

    power: Hook up power handling to redfish
    Signed-off-by: Michael Ellerman <mpe@tenstorrent.com>

--- src/power.c
+++ src/power.c
@@ -29,7 +29,14 @@ static const struct gpio_dt_spec power_gpios[] = {
 #endif
 };
 
-static int power_on(void)
+static bool system_power_state = false;
+
+bool get_power_state(void)
+{
+	return system_power_state;
+}
+
+int power_on(void)
 {
 	int i, ret;
 
@@ -41,10 +48,12 @@ static int power_on(void)
 		}
 	}
 
+	system_power_state = true;
+
 	return 0;
 }
 
-static int power_off(void)
+int power_off(void)
 {
 	int i, ret;
 
@@ -55,6 +64,8 @@ static int power_off(void)
 			return -1;
 		}
 	}
+
+	system_power_state = false;
 
 	return 0;
 }
--- src/power.h
+++ src/power.h
@@ -1,3 +1,7 @@
 int power_init(void);
 int reset_init(void);
 int led_init(void);
+
+int power_on(void);
+int power_off(void);
+bool get_power_state(void);
