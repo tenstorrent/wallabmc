f98add90e70eea6de895aa5d299b27c95e16665f
Joel Stanley <jms@tenstorrent.com>
2025-12-02 00:16:57 +1030

    http: Enable websocket shell
    Enable exporting the console shell and log over websocket.
    
    [np: cut down, also enable ws logging]
    Signed-off-by: Joel Stanley <jms@tenstorrent.com>

--- src/http.c
+++ src/http.c
@@ -11,6 +11,9 @@
 #include <zephyr/net/tls_credentials.h>
 #include <zephyr/net/http/server.h>
 #include <zephyr/net/http/service.h>
+#if defined(CONFIG_SHELL_BACKEND_WEBSOCKET)
+#include <zephyr/shell/shell_websocket.h>
+#endif
 #include <stdio.h>
 
 #include <zephyr/logging/log.h>
@@ -22,6 +25,9 @@ static uint16_t http_service_port = 80;
 HTTP_SERVICE_DEFINE(http_service, NULL, &http_service_port,
 		    2, 5, NULL, NULL, NULL);
 
+#if defined(CONFIG_SHELL_BACKEND_WEBSOCKET)
+DEFINE_WEBSOCKET_SERVICE(http_service);
+#endif
 
 #if defined(CONFIG_APP_HTTPS)
 #include "certificate.h"
@@ -37,6 +43,10 @@ static uint16_t https_service_port = 443;
 HTTPS_SERVICE_DEFINE(https_service, NULL, &https_service_port,
 		     2, 5, NULL, NULL, NULL, sec_tag_list_verify_none,
 		     sizeof(sec_tag_list_verify_none));
+
+#if defined(CONFIG_SHELL_BACKEND_WEBSOCKET)
+DEFINE_WEBSOCKET_SERVICE(https_service);
+#endif
 
 static int setup_tls(void)
 {
@@ -96,6 +106,17 @@ int app_http_server_init(void)
 	err = setup_tls();
 	if (err)
 		return err;
+
+#if defined(CONFIG_SHELL_BACKEND_WEBSOCKET)
+	err = shell_websocket_enable(&GET_WS_SHELL_NAME(http_service));
+	if (err)
+		return err;
+#if defined(CONFIG_APP_HTTPS)
+	err = shell_websocket_enable(&GET_WS_SHELL_NAME(https_service));
+	if (err)
+		return err;
+#endif
+#endif
 
 	return http_server_start();
 }
