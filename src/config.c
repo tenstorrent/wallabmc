tenstorrent/wallabmc

commit 1b9862d131ecc492bd9c8abb9cc49dec0b3da1be

config: config load defaults fix
When the config file is loaded, subsystems that are initialised
afterwards should not be called to apply the config, because they
query the config when initialising.

Remove these redundant / erroenous settings.

Signed-off-by: Nicholas Piggin <npiggin@gmail.com>

src/config.c
@@ -658,54 +658,27 @@ int config_init(void)
 		config_data.version = FS_CONFIG_VERSION;
 	}
 
-	if (IS_ONDISK(bmc_hostname)) {
-		rc = net_do_set_hostname(config_data.bmc_hostname);
-		if (rc) {
-			LOG_ERR("Config: could not set hostname to %s", config_data.bmc_hostname);
-			return rc;
-		}
-		LOG_INF("BMC hostname set to %s", config_data.bmc_hostname);
-	} else {
-		/* This defaults to CONFIG_NET_HOSTNAME */
+	if (!IS_ONDISK(bmc_hostname)) {
+		/* Default to CONFIG_NET_HOSTNAME */
 		strncpy(config_data.bmc_hostname, net_hostname_get(), MAX_HOSTNAME_LEN);
 	}
 
-	if (IS_ONDISK(bmc_use_dhcp4))
-		LOG_INF("BMC DHCPv4: %s", config_data.bmc_use_dhcp4 ? "enabled" : "disabled");
-	else
-		config_data.bmc_use_dhcp4 = 1; /* Default to enabled */
+	if (!IS_ONDISK(bmc_use_dhcp4))
+		config_data.bmc_use_dhcp4 = 1;
 
-	if (IS_ONDISK(bmc_default_ip4)) {
-		if (config_data.bmc_default_ip4) {
-			rc = net_do_set_default_ip4(config_data.bmc_default_ip4);
-			if (rc) {
-				LOG_ERR("Config: could not set default IPv4");
-				return rc;
-			}
-			LOG_INF("BMC default IPv4 set to %s", config_default_ip4_string());
-		}
-	} else {
-		config_data.bmc_default_ip4 = 0; /* Default to not set */
-	}
+	if (!IS_ONDISK(bmc_default_ip4))
+		config_data.bmc_default_ip4 = 0;
 
-	if (IS_ONDISK(host_auto_poweron))
-		LOG_INF("Host auto-poweron: %s", config_data.host_auto_poweron ? "enabled" : "disabled");
-	else
-		config_data.host_auto_poweron = 0; /* Default to disabled */
+	if (!IS_ONDISK(host_auto_poweron))
+		config_data.host_auto_poweron = 0;
 
-	if (!IS_ONDISK(bmc_admin_password)) {
-		/* This defaults to "admin" */
+	if (!IS_ONDISK(bmc_admin_password))
 		strncpy(config_data.bmc_admin_password, "admin", MAX_PW_LEN);
-	}
 
-	if (IS_ONDISK(bmc_use_ntp))
-		LOG_INF("BMC NTP: %s", config_data.bmc_use_dhcp4 ? "enabled" : "disabled");
-	else
-		config_data.bmc_use_ntp = 1; /* Default to enabled */
+	if (!IS_ONDISK(bmc_use_ntp))
+		config_data.bmc_use_ntp = 1;
 
-	if (IS_ONDISK(bmc_ntp_server))
-		LOG_INF("BMC NTP server: %s", config_data.bmc_ntp_server);
-	else
+	if (!IS_ONDISK(bmc_ntp_server))
 		strncpy(config_data.bmc_ntp_server, "pool.ntp.org", MAX_NTP_SERVER_LEN);
 #undef IS_ONDISK
 

commit 2671c35c6bcf8d95a507e0da9d0d572cc8991539

net: Fix BMC net hostname set from config at boot
It turns out some of the configuration application was not so
redundant as previously claimed.

Fixes: 1b9862d131ecc ("config: config load defaults fix")
Signed-off-by: Nicholas Piggin <npiggin@gmail.com>

src/config.c
@@ -68,6 +68,11 @@ BUILD_ASSERT(sizeof(struct config_data) <= 64);
 
 static struct config_data config_data;
 
+const char *config_bmc_hostname(void)
+{
+	return config_data.bmc_hostname;
+}
+
 uint32_t config_bmc_default_ip4(void)
 {
 	return config_data.bmc_default_ip4;

src/config.h
@@ -6,6 +6,7 @@
 #define __CONFIG_H__
 int config_init(void);
 int config_clear(void);
+const char *config_bmc_hostname(void);
 int config_bmc_hostname_set(const char *hostname);
 int config_bmc_password_set(const char *password);
 bool config_bmc_use_ntp(void);

src/net.c
@@ -142,6 +142,11 @@ int net_stop_dhcp4(void)
 int net_init(void)
 {
 	int rc;
+	const char *hostname = config_bmc_hostname();
+
+	rc = net_hostname_set(hostname, strlen(hostname));
+	if (rc)
+		LOG_ERR("Net hostname set failed");
 
 	if (config_bmc_default_ip4()) {
 		LOG_INF("Network static IP set");