fab6ba9a80598567b5f6979d04cc1425741f7ca1
Nicholas Piggin <npiggin@gmail.com>
2026-02-21 17:49:20 +1000

    fs: Use NVS for storage
    Now that Nucleo has 3x32K flash blocks available for app storage and
    P550 has 2x16K blocks, switch to NVS instead of LittleFS (NVS can not
    deal with flash blocks > 64K).
    
    NVS is much smaller in terms of code footprint and requires fewer flash
    storage blocks to work.
    
    For now the fs code now can deal with both littlefs and nvs, but we may
    want to just remove littlefs entirely. When we do that, NVS is a
    key-value store, so config options could be just allocated their own
    keys, then compatibility and upgrading would be simpler and updates
    would require fewer writes to flash.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>

--- src/fs.c
+++ src/fs.c
@@ -8,11 +8,17 @@ LOG_MODULE_REGISTER(wallabmc_fs, LOG_LEVEL_INF);
 
 #include <zephyr/kernel.h>
 #include <zephyr/fs/fs.h>
+#include <zephyr/drivers/flash.h>
+#include <zephyr/storage/flash_map.h>
 
-#include <zephyr/fs/littlefs.h>
+#include "fs.h"
 
-#define STORAGE_PARTITION_LABEL storage_partition
-#define STORAGE_PARTITION_ID FIXED_PARTITION_ID(STORAGE_PARTITION_LABEL)
+#define STORAGE_PARTITION_LABEL		storage_partition
+#if defined(CONFIG_PERSISTENT_STORAGE) && FIXED_PARTITION_EXISTS(STORAGE_PARTITION_LABEL)
+#define STORAGE_PARTITION_ID		FIXED_PARTITION_ID(STORAGE_PARTITION_LABEL)
+#define STORAGE_PARTITION_DEVICE	FIXED_PARTITION_DEVICE(STORAGE_PARTITION_LABEL)
+#define STORAGE_PARTITION_OFFSET	FIXED_PARTITION_OFFSET(STORAGE_PARTITION_LABEL)
+#define STORAGE_PARTITION_SIZE		FIXED_PARTITION_SIZE(STORAGE_PARTITION_LABEL)
 
 static bool fs_is_enabled;
 
@@ -21,8 +27,12 @@ bool fs_enabled(void)
 	return fs_is_enabled;
 }
 
-#if FIXED_PARTITION_EXISTS(STORAGE_PARTITION_LABEL)
+#if defined(CONFIG_FILE_SYSTEM_LITTLEFS)
+#include <zephyr/fs/littlefs.h>
+
 FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(lfs_data);
+
+static const char CONFIG_FILE[] = "/lfs/config_data.bin";
 
 static struct fs_mount_t lfs_mnt = {
 	.type = FS_LITTLEFS,
@@ -58,9 +68,9 @@ static int mount_fs(void)
 		}
 	}
 
-	LOG_INF("Filesystem mounted at %s", lfs_mnt.mnt_point);
-
 	fs_is_enabled = true;
+
+	LOG_INF("Filesystem mounted at %s", lfs_mnt.mnt_point);
 
 	return 0;
 }
@@ -81,6 +91,136 @@ static int umount_fs(void)
 	}
 
 	return 0;
+}
+
+static bool config_exists(void)
+{
+	struct fs_dirent *entry; /* don't put this on stack */
+	int rc;
+
+	if (!fs_enabled())
+		return false;
+
+	entry = malloc(sizeof(*entry));
+
+	rc = fs_stat(CONFIG_FILE, entry);
+	if (rc) {
+		if (rc != -ENOENT)
+			LOG_ERR("Could not stat file %s (err=%d)", CONFIG_FILE, rc);
+		free(entry);
+		return false;
+	}
+
+	if (entry->type != FS_DIR_ENTRY_FILE) {
+		LOG_ERR("Config %s is not a regular file! (err=%d)", CONFIG_FILE, rc);
+		free(entry);
+		return false;
+        }
+
+	free(entry);
+
+	return true;
+}
+
+int config_clear(void)
+{
+	int rc;
+
+	if (!config_exists())
+		return 0; /* Already cleared */
+
+	rc = fs_unlink(CONFIG_FILE);
+	if (rc) {
+		LOG_ERR("Could not remove file %s (err=%d)", CONFIG_FILE, rc);
+		return rc;
+	}
+
+	return 0;
+}
+
+ssize_t config_read(void *buf, size_t size)
+{
+	struct fs_file_t config_file;
+	size_t remain = size;
+	size_t copied = 0;
+	int rc;
+
+	fs_file_t_init(&config_file);
+
+	if (!config_exists()) {
+		rc = fs_open(&config_file, CONFIG_FILE, FS_O_CREATE);
+		if (rc) {
+			LOG_ERR("Could not create file %s (err=%d)", CONFIG_FILE, rc);
+			return rc;
+		}
+		memset(buf, 0, size);
+		goto out_close;
+	}
+
+	rc = fs_open(&config_file, CONFIG_FILE, FS_O_READ);
+	if (rc) {
+		LOG_ERR("Could not open or create file %s (err=%d)", CONFIG_FILE, rc);
+		return rc;
+	}
+
+	memset(buf, 0, size);
+
+	while (remain) {
+		rc = fs_read(&config_file, buf + copied, remain);
+		if (rc < 0) {
+			LOG_ERR("Could not read file %s (err=%d)", CONFIG_FILE, rc);
+			return rc;
+		}
+		if (rc == 0)
+			break;
+
+		remain -= rc;
+		copied += rc;
+	}
+
+out_close:
+	rc = fs_close(&config_file);
+	if (rc) {
+		LOG_ERR("Could not close file %s (err=%d)", CONFIG_FILE, rc);
+		return rc;
+	}
+
+	return copied;
+}
+
+ssize_t config_write(const void *buf, size_t size)
+{
+	struct fs_file_t config_file;
+	size_t remain = size;
+	size_t copied = 0;
+	int rc;
+
+	fs_file_t_init(&config_file);
+
+	rc = fs_open(&config_file, CONFIG_FILE, FS_O_WRITE);
+	if (rc) {
+		LOG_ERR("Could not open file %s (err=%d)", CONFIG_FILE, rc);
+		return rc;
+	}
+
+	while (remain) {
+		rc = fs_write(&config_file, buf + copied, remain);
+		if (rc <= 0) {
+			LOG_ERR("Could not write file %s (err=%d)", CONFIG_FILE, rc);
+			return rc;
+		}
+
+		remain -= rc;
+		copied += rc;
+	}
+
+	rc = fs_close(&config_file);
+	if (rc) {
+		LOG_ERR("Could not close file %s (err=%d)", CONFIG_FILE, rc);
+		return rc;
+	}
+
+	return copied;
 }
 
 #define BUF_SIZE 32
@@ -140,12 +280,133 @@ static int increment_boot_file(void)
 	return 0;
 }
 
+#elif defined(CONFIG_NVS) /* CONFIG_FILE_SYSTEM_LITTLEFS */
+#include <zephyr/kvss/nvs.h>
+
+static const int CONFIG_ID = 1;
+
+static struct nvs_fs nvs_fs = {
+	.flash_device = STORAGE_PARTITION_DEVICE,
+	.offset = STORAGE_PARTITION_OFFSET,
+};
+
+static int mount_fs(void)
+{
+	struct flash_pages_info info;
+	int rc;
+
+	LOG_INF("Mounting NVS...");
+
+	rc = flash_get_page_info_by_offs(nvs_fs.flash_device, nvs_fs.offset, &info);
+	if (rc) {
+		LOG_ERR("Unable to get flash info (err=%d)", rc);
+		return rc;
+	}
+
+	nvs_fs.sector_size = info.size;
+	nvs_fs.sector_count = STORAGE_PARTITION_SIZE / nvs_fs.sector_size;
+	LOG_INF("  NVS using flash at 0x%08lx sector size 0x%08x sector count %u",
+		nvs_fs.offset, nvs_fs.sector_size, nvs_fs.sector_count);
+
+	/*
+	 * CONFIG_NVS_INIT_BAD_MEMORY_REGION is set, so this will try to init
+	 * storage if there was no NVS filesystem there.
+	 */
+	rc = nvs_mount(&nvs_fs);
+	if (rc) {
+		LOG_DBG("Mount failed (err=%d)", rc);
+		return rc;
+	}
+
+	fs_is_enabled = true;
+
+	LOG_INF("NVS mounted");
+
+	return 0;
+}
+
+static int umount_fs(void)
+{
+	fs_is_enabled = false;
+
+	return 0;
+}
+
+int config_clear(void)
+{
+	int rc;
+
+	if (!fs_enabled())
+		return 0;
+
+	rc = nvs_clear(&nvs_fs);
+	if (rc) {
+		LOG_ERR("Could not clear config (err=%d)", rc);
+		return rc;
+	}
+
+	return 0;
+}
+
+ssize_t config_read(void *buf, size_t size)
+{
+	int rc;
+
+	memset(buf, 0, size);
+
+	rc = nvs_read(&nvs_fs, CONFIG_ID, buf, size);
+	if (rc < 0) {
+		if (rc == -ENOENT)
+			return 0;
+		LOG_ERR("Could not read config (err=%d)", rc);
+		return rc;
+	}
+
+	if (rc > size) {
+		LOG_INF("Read newer version of config (size=%d)", rc);
+		rc = size;
+	}
+
+	return rc;
+}
+
+ssize_t config_write(const void *buf, size_t size)
+{
+	int rc;
+
+	rc = nvs_write(&nvs_fs, CONFIG_ID, buf, size);
+	if (rc < 0) {
+		LOG_ERR("Could not write config (err=%d)", rc);
+		return rc;
+	}
+	if (rc == 0) {
+		LOG_DBG("Writing unchanged data");
+		return size;
+	}
+
+	return rc;
+}
+
+static int increment_boot_file(void)
+{
+	return -ENOTSUP;
+}
+
+#else /* CONFIG_FILE_SYSTEM_LITTLEFS */
+#error "No storage system defined (LITTLEFS or NVS)"
+#endif /* CONFIG_FILE_SYSTEM_LITTLEFS */
+
 /* Useful for debugging persistence, set this 0 to reduce flash writes */
 #define BOOT_COUNTER 0
 
 int fs_init(void)
 {
 	int rc;
+
+	if (!device_is_ready(STORAGE_PARTITION_DEVICE)) {
+		LOG_ERR("Flash device not ready");
+		return -ENODEV;
+	}
 
 	rc = mount_fs();
 	if (rc)
@@ -166,7 +427,8 @@ int fs_exit(void)
 {
 	return umount_fs();
 }
-#else /* FIXED_PARTITION_EXISTS(STORAGE_PARTITION_LABEL) */
+#else /* defined(CONFIG_PERSISTENT_STORAGE) && FIXED_PARTITION_EXISTS(STORAGE_PARTITION_LABEL) */
+
 int fs_init(void)
 {
 	LOG_INF("Filesystem storage not enabled for this board");
@@ -178,4 +440,25 @@ int fs_exit(void)
 	LOG_INF("Filesystem storage not enabled for this board");
 	return 0;
 }
-#endif /* FIXED_PARTITION_EXISTS(STORAGE_PARTITION_LABEL) */
+
+bool fs_enabled(void)
+{
+	return false;
+}
+
+ssize_t config_read(void *buf, size_t size)
+{
+	return -ENODEV;
+}
+
+ssize_t config_write(const void *buf, size_t size)
+{
+	return -ENODEV;
+}
+
+int config_clear(void)
+{
+	return 0;
+}
+
+#endif /* defined(CONFIG_PERSISTENT_STORAGE) && FIXED_PARTITION_EXISTS(STORAGE_PARTITION_LABEL) */
--- src/fs.h
+++ src/fs.h
@@ -9,10 +9,16 @@
 int fs_init(void);
 int fs_exit(void);
 bool fs_enabled(void);
+ssize_t config_read(void *buf, size_t size);
+ssize_t config_write(const void *buf, size_t size);
+int config_clear(void);
 #else
 static inline int fs_init(void) { return 0; }
 static inline int fs_exit(void) { return 0; }
 static inline bool fs_enabled(void) { return false; }
+static inline ssize_t config_read(void *buf, size_t size) { return -EIO; }
+static inline ssize_t config_write(const void *buf, size_t size) { return -EIO; }
+static inline int config_clear(void) { return 0; }
 #endif
 
 #endif
